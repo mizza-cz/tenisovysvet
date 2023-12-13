867eae4a917a0a9b220e01d99edd9f654c2adb2b
mizza-cz <mizza.cz666@gmail.com>
2023-12-13 16:09:44 +0100

    add fix galerie

--- src/js/others/lightgallery.js
+++ src/js/others/lightgallery.js
@@ -1,20 +1,30 @@
 // Light Gallery
 $('#animated-thumbnail').lightGallery({
   thumbnail: true,
+  download: false,
+  customSlideName: true,
   getCaptionFromTitleOrAlt: true,
   selector: 'a[style]',
-})
+});
 // Image Transition
 var scroll = 'yes',
-  Fscroll = scroll.replace(/(\r\n|\n|\r)/gm, ' ')
+  Fscroll = scroll.replace(/(\r\n|\n|\r)/gm, ' ');
 'yes' === Fscroll &&
   ($(document).ready(function () {
-    $('body').addClass('imgani')
+    $('body').addClass('imgani');
   }),
   $(window).bind('load resize scroll', function () {
-    var o = $(this).height()
+    var o = $(this).height();
     $('img').each(function () {
-      var s = 0.1 * $(this).height() - o + $(this).offset().top
-      $(document).scrollTop() > s && $(this).addClass('anime')
-    })
-  }))
+      var s = 0.1 * $(this).height() - o + $(this).offset().top;
+      $(document).scrollTop() > s && $(this).addClass('anime');
+    });
+  }));
+
+
+
+
+
+
+
+  
