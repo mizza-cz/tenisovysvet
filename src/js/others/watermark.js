bdba28a3f4d794090978ff255ee64fa27624a1ae
mizza-cz <mizza.cz666@gmail.com>
2023-12-13 18:24:09 +0100

    watermark

---
+++ src/js/others/watermark.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.galleryArticle__img').watermark({
+    text: 'Tenisovysvet.cz        ',
+    textWidth: 250,
+    opacity: 1,
+    margin: 0,
+    textSize: 24,
+  });
+});
