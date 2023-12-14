c51c4f2cac30ac30f9f859744690bc2f42815c55
mizza-cz <mizza.cz666@gmail.com>
2023-12-14 12:29:01 +0100

    add fix

--- dist/js/others/watermark.js
+++ dist/js/others/watermark.js
@@ -8,12 +8,28 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function () {
-  $('.galleryArticle__item a').watermark({
-    text: 'Tenisovysvet.cz        ',
-    textWidth: 250,
-    opacity: 1,
-    margin: 0,
-    textSize: 24,
-  });
-});
+document.addEventListener(
+  'contextmenu',
+  function (e) {
+    e.preventDefault();
+  },
+  false,
+);
+
+document.addEventListener(
+  'dragstart',
+  function (e) {
+    e.preventDefault();
+  },
+  false,
+);
+
+document.addEventListener(
+  'keydown',
+  function (e) {
+    if (e.ctrlKey && (e.key === 's' || e.key === 'c')) {
+      e.preventDefault();
+    }
+  },
+  false,
+);
